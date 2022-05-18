23ad883782803674a9fc5371e80fcfaf093fe1de
Sharapooff <sharapooff@mail.ru>
2022-05-18 15:26:21 +0300

    Добавить файлы проекта и поправить gitIgnore
     - файлы проекта добавлены;
     - GitIgnore подправлен.

---
+++ EFCoreCodeSecond/Program.cs
@@ -0,0 +1,14 @@
+﻿using EFCoreCodeSecond;
+
+Console.WriteLine("CodeSecond_GO");
+
+using(UsersContext db = new UsersContext())
+{
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("Список объектов:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
