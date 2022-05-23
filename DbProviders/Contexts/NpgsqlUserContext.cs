073d77131288e20f8c646fb089c856c1384c6d1c
Sharapooff <sharapooff@mail.ru>
2022-05-23 15:12:34 +0300

    Добавить примеры работы с раличными провайдорами данных
     - примеры добавлены для SqlServer, MySql, PostgreSql

---
+++ DbProviders/Contexts/NpgsqlUserContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using DbProviders.Models;
+
+namespace DbProviders.Contexts
+{
+    internal class NpgsqlUserContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>(); //{ get; set; } = null!; //{ get; set; }
+        
+        //передача в конструктор базового класса объекта DbContextOptions,
+        //который инкапсулирует параметры конфигурации
+        public NpgsqlUserContext(DbContextOptions<NpgsqlUserContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        //или через OnConfiguring
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=пароль_от_postgres");
+        //}
+    }
+}
