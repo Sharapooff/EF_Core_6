6d7bd951438ad294411d6468f3bdc5dbc168c638
Sharapooff <sharapooff@mail.ru>
2022-05-31 15:27:08 +0300

    Добавить проект с примером Parallelism

---
+++ Parallelism/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallelism.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        [Timestamp] //параллелизм //using System.ComponentModel.DataAnnotations;
+        public byte[] Timestamp { get; set; }
+    }
+}
